c30574eb618228dd65772ec759d5097a27907f27
Dante Piazza Quiroga <dante.7h@gmail.com>
2024-06-21 12:46:55 -0300

    Update endpoints

--- src/Endpoints/Contacts.php
+++ src/Endpoints/Contacts.php
@@ -7,5 +7,48 @@ class Contacts {
 	function __construct($adapter){
 		$this -> adapter = $adapter;
 	}
+	
+	/**
+	 * Create a new contact
+	 * 
+	 * @since 1.0
+	 *
+	 * @param array $options
+	 *
+	 * @return Dolibarr\Adapter\Response
+	 **/
+	public function Create(array $options) {
+		$this -> adapter -> payload = $options;
+
+		return $this -> adapter -> exec('post', 'contacts') -> response;
+	}
+	
+	/**
+	 * Update a contact
+	 * 
+	 * @since 1.0
+	 *
+	 * @param array $options
+	 *
+	 * @return Dolibarr\Adapter\Response
+	 **/
+	public function Update(int $thirdparty, array $options) {
+		$this -> adapter -> payload = $options;
+
+		return $this -> adapter -> exec('put', 'contacts/'.$thirdparty) -> response;
+	}
+
+	/**
+	 * List contacts
+	 * 
+	 * @since 1.0
+	 *
+	 * @param array $options
+	 *
+	 * @return Dolibarr\Adapter\Response
+	 **/
+	public function All(array $options = []) {
+		return $this -> adapter -> exec('get', 'contacts', $options) -> response;
+	}
 }
 
--- src/Endpoints/Thirdparties.php
+++ src/Endpoints/Thirdparties.php
@@ -22,6 +22,21 @@ class Thirdparties {
 
 		return $this -> adapter -> exec('post', 'thirdparties') -> response;
 	}
+	
+	/**
+	 * Update a thirdpart
+	 * 
+	 * @since 1.0
+	 *
+	 * @param array $options
+	 *
+	 * @return Dolibarr\Adapter\Response
+	 **/
+	public function Update(int $thirdparty, array $options) {
+		$this -> adapter -> payload = $options;
+
+		return $this -> adapter -> exec('put', 'thirdparties/'.$thirdparty) -> response;
+	}
 
 	/**
 	 * List thirdparties
